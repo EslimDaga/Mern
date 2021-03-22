a683c76839ee1b0c1d43c6c73d1ed6460d02811c
Eslim Daga <esliim9811@gmail.com>
2021-03-22 16:53:27 -0500

    Add routes

--- client/src/index.js
+++ client/src/index.js
@@ -2,8 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter,Route,Redirect,Switch } from "react-router-dom";
+import Register from "./Screens/Register";
 
 ReactDOM.render(
-    <App />,
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact render={props => <App {...props} />}/>
+        <Route path="/register" exact render={props => <Register {...props} />}/>
+      </Switch>
+    </BrowserRouter>,
   document.getElementById("root")
 );
