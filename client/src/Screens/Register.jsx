5c1c5efb4e76d9a7390a3eceff78c52fbf0933ee
Eslim Daga <esliim9811@gmail.com>
2021-03-22 16:51:04 -0500

    Basic configuration

--- client/src/Screens/Register.jsx
+++ client/src/Screens/Register.jsx
@@ -1,3 +1,16 @@
 import React, { useState } from "react";
+import axios from "axios";
+import { Redirect } from "react-router-dom";
 import authSvg from "../assets/auth.svg";
 import { ToastContainer, toast } from "react-toastify";
+import { authenticate, isAuth } from "../helpers/auth";
+
+const Register = () => {
+  return(
+    <div>
+      Register Page
+    </div>
+  )
+};
+
+export default Register;
