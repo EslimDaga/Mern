f99d61a5aa7ce1a1701e97ebdc8cd16984263647
Eslim Daga <esliim9811@gmail.com>
2021-03-22 17:48:41 -0500

    Add react toastify styles

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,9 @@ import App from "./App";
 import { BrowserRouter,Route,Redirect,Switch } from "react-router-dom";
 import Register from "./Screens/Register";
 
+import "react-toastify/dist/ReactToastify.css";
+
+
 ReactDOM.render(
     <BrowserRouter>
       <Switch>
