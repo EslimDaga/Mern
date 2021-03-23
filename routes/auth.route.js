e031db689246cd8839ac7c422ed3cb263f286cdb
Eslim Daga <esliim9811@gmail.com>
2021-03-23 12:08:57 -0500

    Add validation in the routes

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
-//Load controllers
-const { registerController } = require("../controllers/auth.controller");
+//Validation
+const { validRegister,validLogin,forgotPasswordValidator,resertPasswordValidator } = require("../helpers/valid");
 
-router.post("/register", registerController);
+//Load controllers
+const { registerController, activationController } = require("../controllers/auth.controller");
+
+router.post("/register", validRegister, registerController);
+router.post("/activation", activationController);
 
 module.exports = router;
