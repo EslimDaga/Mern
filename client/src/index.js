c681bfdee66a21f25e3b0a2e70d633c363bec852
Eslim Daga <esliim9811@gmail.com>
2021-03-23 22:39:42 -0500

    Add route Login

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter,Route,Redirect,Switch } from "react-router-dom";
 import Register from "./Screens/Register";
+import Login from "./Screens/Login";
 import Activate from "./Screens/Activate";
 
 import "react-toastify/dist/ReactToastify.css";
@@ -13,6 +14,7 @@ ReactDOM.render(
       <Switch>
         <Route path="/" exact render={props => <App {...props} />}/>
         <Route path="/register" exact render={props => <Register {...props} />}/>
+        <Route path="/login" exact render={props => <Login {...props} />} />
         <Route path="/users/activate/:token" exact render={props => <Activate {...props} />} />
       </Switch>
     </BrowserRouter>,
