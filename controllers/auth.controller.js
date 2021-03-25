26cf55d37ab655cda260b13f17473ee86c0060c7
Eslim Daga <esliim9811@gmail.com>
2021-03-25 15:58:43 -0500

    Add forget controller

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -70,7 +70,7 @@ exports.registerController = (req,res) => {
       })
     })
   }
-}
+};
 
 //Register for Backend done let's create for it
 
@@ -117,7 +117,7 @@ exports.activationController = (req,res) => {
       message: "Error happening please try again"
     })
   }
-}
+};
 
 exports.loginController = (req,res) => {
   const { email, password } = req.body;
@@ -159,5 +159,133 @@ exports.loginController = (req,res) => {
       const { _id, name, email, role } = user;
       return res.json({ token, user : { _id, name, email, role }});
     });
+  }
+};
+
+exports.forgetController = (req, res) => {
+  const { email } = req.body;
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    const firstError = errors.array().map(error => error.msg)[0];
+    return res.status(422).json({
+      errors: firstError
+    });
+  } else {
+    User.findOne(
+      {
+        email
+      },
+      (err, user) => {
+        if (err || !user) {
+          return res.status(400).json({
+            error: 'User with that email does not exist'
+          });
+        }
+
+        const token = jwt.sign(
+          {
+            _id: user._id
+          },
+          process.env.JWT_RESET_PASSWORD,
+          {
+            expiresIn: '10m'
+          }
+        );
+
+        const emailData = {
+          from: process.env.EMAIL_FROM,
+          to: email,
+          subject: `Password Reset link`,
+          html: `
+                    <h1>Please use the following link to reset your password</h1>
+                    <p>${process.env.CLIENT_URL}/users/password/reset/${token}</p>
+                    <hr />
+                    <p>This email may contain sensetive information</p>
+                    <p>${process.env.CLIENT_URL}</p>
+                `
+        };
+
+        return user.updateOne(
+          {
+            resetPasswordLink: token
+          },
+          (err, success) => {
+            if (err) {
+              console.log('RESET PASSWORD LINK ERROR', err);
+              return res.status(400).json({
+                error:
+                  'Database connection error on user password forgot request'
+              });
+            } else {
+              sgMail
+                .send(emailData)
+                .then(sent => {
+                  // console.log('SIGNUP EMAIL SENT', sent)
+                  return res.json({
+                    message: `Email has been sent to ${email}. Follow the instruction to activate your account`
+                  });
+                })
+                .catch(err => {
+                  // console.log('SIGNUP EMAIL SENT ERROR', err)
+                  return res.json({
+                    message: err.message
+                  });
+                });
+            }
+          }
+        );
+      }
+    );
+  }
+};
+
+exports.resetController = (req,res) => {
+  const { resetPasswordLink, newPassword } = req.body;
+  const error = validationResult(req);
+
+  //Validation to req.body we will create custom validation in seconds
+  if(!error.isEmpty()){
+    const firstError = error.array().map(error => error.msg)[0]
+    return res.status(422).json({
+      error : firstError
+    });
+  }else{
+    if(resetPasswordLink){
+      jwt.verify(resetPasswordLink, process.env.JWT_RESET_PASSWORD, function(err,decoded){
+        if(err){
+          return res.status(400).json({
+            error : "Expired Link, try again"
+          });
+        }
+
+        User.findOne({ resetPasswordLink }, (err,user) => {
+          if(err || !user){
+            return res.status(400).json({
+              error : "Something went wrong, Try later"
+            });
+          }
+
+          const updateFields = {
+            password : newPassword,
+            resetPasswordLink : ""
+          }
+
+          user = _.extend(user, updateFields);
+
+          user.save((err,result) => {
+            if(err){
+              return res.status(400).json({
+                error : "Error reseting user password"
+              });
+            }
+
+            res.json({
+              message : "Great! Now you can login with new password"
+            });
+          });
+        });
+      });
+    }
   }
 }
