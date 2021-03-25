6752116b064d3b06dd0f7ad32b9f138325aab4bd
Eslim Daga <esliim9811@gmail.com>
2021-03-25 15:59:54 -0500

    Add routes forget and reset password

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -5,10 +5,18 @@ const router = express.Router();
 const { validRegister,validLogin,forgotPasswordValidator,resertPasswordValidator } = require("../helpers/valid");
 
 //Load controllers
-const { registerController, activationController, loginController } = require("../controllers/auth.controller");
+const {
+  registerController,
+  activationController,
+  loginController,
+  forgetController,
+  resetController
+} = require("../controllers/auth.controller");
 
 router.post("/register", validRegister, registerController);
 router.post("/login", validLogin, loginController);
 router.post("/activation", activationController);
+router.put("/password/forget", forgotPasswordValidator, forgetController);
+router.put("/password/reset", resertPasswordValidator, resetController);
 
 module.exports = router;
