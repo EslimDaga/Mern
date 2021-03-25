8b28d3e311de2d2130cf1dcf042e12fef230cbbb
Eslim Daga <esliim9811@gmail.com>
2021-03-25 16:03:14 -0500

    Add routes forget and reset

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { BrowserRouter,Route,Redirect,Switch } from "react-router-dom";
+import { BrowserRouter,Route,Switch } from "react-router-dom";
 import Register from "./Screens/Register";
 import Login from "./Screens/Login";
+import Forget from "./Screens/Forget";
 import Activate from "./Screens/Activate";
+import Reset from "./Screens/Reset";
 
 import "react-toastify/dist/ReactToastify.css";
 
@@ -16,6 +18,8 @@ ReactDOM.render(
         <Route path="/register" exact render={props => <Register {...props} />}/>
         <Route path="/login" exact render={props => <Login {...props} />} />
         <Route path="/users/activate/:token" exact render={props => <Activate {...props} />} />
+        <Route path="/users/password/forget" exact render={props => <Forget {...props} />} />
+        <Route path="/users/password/reset/:token" exact render={props => <Reset {...props} />} />
       </Switch>
     </BrowserRouter>,
   document.getElementById("root")
