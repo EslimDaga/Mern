432d0d7b32388a760edc8bac56a5af4d7d692b19
Eslim Daga <esliim9811@gmail.com>
2021-03-25 18:28:04 -0500

    Add route google login

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -10,7 +10,8 @@ const {
   activationController,
   loginController,
   forgetController,
-  resetController
+  resetController,
+  googleController
 } = require("../controllers/auth.controller");
 
 router.post("/register", validRegister, registerController);
@@ -18,5 +19,8 @@ router.post("/login", validLogin, loginController);
 router.post("/activation", activationController);
 router.put("/password/forget", forgotPasswordValidator, forgetController);
 router.put("/password/reset", resertPasswordValidator, resetController);
+
+//Implements social login google and facebook
+router.post("/googlelogin", googleController);
 
 module.exports = router;
